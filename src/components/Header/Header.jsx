629c9e3f400e50db754edf943b45d4138d9bb108
Owen Patterson <owen.patt.8@gmail.com>
2023-11-27 17:18:19 -0500

    Started Header and Nav components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
+import "./Header.css";
 import Nav from "../Nav/Nav";
 
 const Header = () => {
   return (
-    <header>
+    <header className="header">
       <Nav></Nav>
+      <div className="header__content">
+        <h1 className="header__title">
+          Hello, I'm <span className="header__name">Owen Patterson</span>
+        </h1>
+        <h2 className="header__subtitle">React.js Developer</h2>
+      </div>
     </header>
   );
 };
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/logo-white.png";
 import Switch from "../Switch/Switch";
 import "./Nav.css";
 
