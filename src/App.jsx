7b23e064f681b130326a78847836203b7d1df6bb
Owen Patterson <owen.patt.8@gmail.com>
2023-11-27 18:53:53 -0500

    Started setting up react routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Home from "./components/Home/Home";
 import Nav from "./components/Nav/Nav";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="page">
       <Nav></Nav>
-      <Home></Home>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
