ae12c047c318bda48b747c82a418d8b13630e375
Owen Patterson <owen.patt.8@gmail.com>
2023-11-28 22:16:46 -0500

    Kept working on the resume page.

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -38,20 +38,63 @@ const Resume = () => {
       </section>
       <section className="resume__section">
         <h3 className="resume__subtitle">Technical Projects</h3>
-        <p className="resume__text resume__text_left">
-          WTWR (What to Wear?) -{" "}
-          <a
-            className="resume__link"
-            href="https://github.com/owenPatt/se_project_react">
-            Github
-          </a>{" "}
-          |{" "}
-          <a
-            className="resume__link"
-            href="https://owenpatt.github.io/se_project_react/">
-            Pages
-          </a>{" "}
-        </p>
+        <div className="resume__tech-project">
+          <p className="resume__text resume__text_left">
+            WTWR (What to Wear?) -{" "}
+            <a
+              className="resume__link"
+              href="https://github.com/owenPatt/se_project_react">
+              Github
+            </a>{" "}
+            |{" "}
+            <a
+              className="resume__link"
+              href="https://owenpatt.github.io/se_project_react/">
+              Pages
+            </a>{" "}
+            |{" "}
+            <a className="resume__link" href="https://openweathermap.org/">
+              API
+            </a>{" "}
+            | React, JavaScript, HTML/CSS, OpenWeatherMap API | 2023
+          </p>
+          <p className="resume__text resume__text_left">
+            A React application that displays the weather forecast for a given
+            location and provides clothing recommendations based on the weather,
+            which it gets through OpenWeatherMap API.
+          </p>
+          <ul className="resume__list">
+            <li className="resume__list-item">
+              Uses React to create a single page application, which looks and
+              feels nice to use.
+            </li>
+            <li className="resume__list-item">
+              Uses OpenWeatherMap API to get weather data for a given location.
+              The weather also changes the image and color of the banner.
+            </li>
+            <li className="resume__list-item">
+              Next steps is intergrating user log in to allow for
+              personalization and saving of location/cards.
+            </li>
+          </ul>
+        </div>
+        <div className="resume__tech-project">
+          <p className="resume__text resume__text_left">
+            SirGrungle The Bot -{" "}
+            <a
+              className="resume__link"
+              href="https://github.com/owenPatt/SirGrungleBot">
+              Github
+            </a>{" "}
+            | Node.js, JavaScript, Discord API | 2023
+          </p>
+          <p className="resume__text resume__text_left"></p>
+          <ul className="resume__list">
+            <li className="resume__list-item"></li>
+            <li className="resume__list-item"></li>
+            <li className="resume__list-item"></li>
+          </ul>
+        </div>
       </section>
     </div>
   );
