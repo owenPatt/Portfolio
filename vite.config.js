637412bfde11c5da527d256d795e8b433a4158a8
Owen Michael Patterson <61700698+owenPatt@users.noreply.github.com>
2025-04-13 11:22:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/student_files/opatterson/comw282/portfolio/",
+  base: "/portfolio/",
 });
